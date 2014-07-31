bda860513355089f3f6ec1babd3575d132db53ae
Sharell Bryant <sharell.bryant@gmail.com>
2014-07-31 13:38:30 +0000

    Added text-size button

--- inc/header.php
+++ inc/header.php
@@ -2,6 +2,9 @@
 <head>
 	<link rel="stylesheet"  href="css/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Poiret+One|Cinzel+Decorative:400,700' rel='stylesheet' type='text/css'>
+	<script src="scripts/jquery-1.11.1.min.js"></script>
+	<script src="scripts/blog.js"></script>
+
 	<title>Sharell Bryant - Writing</title>
 </head>
 <body>
---
+++ scripts/blog.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	$('#text-icon').click( 
+		function() {$('p').css('font-size', '25px');}
+	)
+
+});
